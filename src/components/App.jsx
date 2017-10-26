905e1a8a1ccedcaac77018e90705cecc08ddd6dd
andino93 <andy.andino93@gmail.com>
2017-10-25 20:29:04 -0700

    add working code for example data and api call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,45 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      currentVideo: exampleVideoData[1],
+      videoCollection: exampleVideoData,
+      searchQuery: 'cats'
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search search={this.search.bind(this)}/></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={this.state.currentVideo} /></div>
+          </div>
+          <div className="col-md-5">
+            <div ><VideoList videos={this.state.videoCollection} 
+              videoEntryClick={this.videoEntryClick.bind(this)}/></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+  
+  videoEntryClick(video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+  
+  search(value) {
+    console.log(value);
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  let data = 
+  
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      type: 'video'    
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(data) {
+      console.log('FAILED REQUEST: ', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
